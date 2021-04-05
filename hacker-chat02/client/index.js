5bd7aa49609e58d98ce1fe22635e95e6ed8d75f7
unknown <lucas46em@gmail.com>
2021-04-05 18:07:30 -0300

    Day 2 - configured socket server

---
+++ hacker-chat02/client/index.js
@@ -0,0 +1,22 @@
+/**
+ * node index.js\
+ *      --username lucas
+ *      --room sala01
+ *      --hostUri localhost
+ * / */
+
+
+import Events from 'events'
+import CliConfig from './src/cliConfig.js'
+import SocketClient from './src/socket.js'
+import TerminalController from "./src/terminalController.js"
+
+const [nodePath, filePath, ...commands ] = process.argv
+const config = CliConfig.parseArguments(commands)
+
+const componentEmitter = new Events()
+
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
---
+++ hacker-chat02/client/src/constants.js
@@ -0,0 +1,14 @@
+export const constants = {
+    events:{
+        app: {
+            MESSAGE_SENT: 'message:sent',
+            MESSAGE_RECEIVEID: 'message:receveid',
+            ACTIVITYLOG_UPDATED:'activityLog:updated',
+            STATUS_UPDATED: 'status:updated'
+        },
+
+        socket:{
+
+        }
+    }
+}
